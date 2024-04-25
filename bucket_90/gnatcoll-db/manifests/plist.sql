a65d3261d16f2dc0cf75ee0bbaa75952acd9c0e5
John Marino <draco@marino.st>
2024-04-25 20:11:08 +0000

    add gnatcoll-db (somehow it got skipped)

---
+++ bucket_90/gnatcoll-db/manifests/plist.sql
@@ -0,0 +1,4 @@
+lib/libgnatcoll_sql.so.%%SOMAJOR%%
+lib/libgnatcoll_sql.so.%%SOVERSION%%
+lib/gnatcoll_sql.relocatable/libgnatcoll_sql.so.%%SOMAJOR%%
+lib/gnatcoll_sql.relocatable/libgnatcoll_sql.so.%%SOVERSION%%
