78f02b0ee48a9187d37cfc8a56fe8408029f314e
John Marino <draco@marino.st>
2024-10-23 18:19:41 +0000

    save WIP for next version of gnatstudio

--- bucket_CD/gnatcoll-core/patches/patch-src_os_unix_process-wrappers.c
+++ bucket_CD/gnatcoll-core/patches/patch-src_os_unix_process-wrappers.c
@@ -1,4 +1,4 @@
---- src/os/unix/process-wrappers.c.orig	2023-09-21 15:57:14 UTC
+--- src/os/unix/process-wrappers.c.orig	2024-04-08 08:32:00 UTC
 +++ src/os/unix/process-wrappers.c
 @@ -28,6 +28,10 @@
  #include <unistd.h>
